4efe7ee3a9638320ff03ea89133e350ebe5cfd8d
23younesm <64438560+23younesm@users.noreply.github.com>
2025-02-16 20:26:32 -0500

    HW

--- src/my-card.js
+++ src/my-card.js
@@ -14,7 +14,10 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "#";
+    this.image = "#";
+    this.link = "#"; 
+    this.fancy = false;
   }
 
   static get styles() {
@@ -22,16 +25,106 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+
+      :host([fancy]) .card{
+        display: block;
+        background-color: blue;
+        color: white
+      }
+
+      .card.toggled {
+        background-color: red;
+        color: blue;
+      }
+      .card {
+        background-color: #060c4f;
+        width: 400px;
+        text-align: center;
+      }
+      .cardheader {
+        color: yellow;
+        margin: 10px 0px 10px 0px;
+        font-size: 40px;
+        font-family: "Times New Roman", Times, serif;
+      }
+      p {
+        color: yellow;
+        margin: 10px 25px 10px 25px;
+        font-family: "Times New Roman", Times, serif;
+      }
+      img {
+        width: 200px;
+        border: 4px solid yellow;
+      }
+      .btn {
+        color: #4e064f;
+        background-color: white;
+        font-size: 15px;
+        margin: 0px 0px 5px 0px;
+      }
+      .btn:focus,
+      .btn:hover {
+        background-color: blue;
+        color: white;
+      }
+      details summary {
+      text-align: center;
+      font-size: 20px;
+      padding: 8px 0;
+      font-family: "Times New Roman", Times, serif;
+      color: yellow;
+
+      }
+
+    details[open] summary {
+      font-weight: bold;
+     }
+  
+    details div {
+      border: 2px solid black;
+      text-align: center;
+      padding: 8px;
+      height: 70px;
+      overflow: auto;
+      }
     `;
   }
 
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
+  }
+
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    <div id = "cardlist">
+      <div class="card">
+        <h1 class="cardheader"><b>${this.title}</b></h1>
+          <img src=${this.image} alt=${this.title} />
+          <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+            <summary>Description</summary>
+            <div>
+              <slot></slot>
+              <a href=${this.link} target="_blank">
+                <button class="btn"><em>Link for more info</em></button>
+              </a>
+            </div>
+          </details>
+      </div>
+    </div>`;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      image: { type: String },
+      link: { type: String},
+      fancy: { type: Boolean, reflect: true }
     };
   }
 }
